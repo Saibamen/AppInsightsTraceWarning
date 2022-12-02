b454dfd45c708431f1e2ef92e406b4f8ec839c2a
Adam Stachowicz <adam.stachowicz@fingo.info>
2022-12-02 08:40:45 +0100

    Fix build

--- AppInsightsTraceWarning/Startup.cs
+++ AppInsightsTraceWarning/Startup.cs
@@ -69,7 +69,6 @@ namespace AppInsightsTraceWarning
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostEnvironment env, TelemetryConfiguration telemetryConfiguration)
         {
-            InternalLoggerConfiguration.Configure(env, Configuration);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
